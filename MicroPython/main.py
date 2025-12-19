30188ff09e6f74a578c1141a09359f3cd2dac49b
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-12-19 17:41:36 +0000

    completed python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,29 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Isaac Ip
+Created on: Dec 2025
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+# setup
+display.clear()
+display.show(Image.HAPPY)
+pin0.set_analog_period(20)
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        pin0.write_analog(50)
+        display.scroll(0)
+        display.scroll(Image.SQUARE_SMALL)
+        sleep(1000)  # Wait for 1 second
+        display.clear()
+        display.show(Image.HAPPY)
+
+    if button_b.is_pressed():
+        pin0.write_analog(230)
+        display.scroll(180)
+        display.scroll(Image.SQUARE_SMALL)
+        sleep(1000)  # Wait for 1 second
+        display.clear()
+        display.show(Image.HAPPY)
